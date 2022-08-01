de3274a6110fd764e0fcf4cb5e0f476bf3596b2c
iñakooo <iako05rivas@gmail.com>
2022-08-01 11:56:10 -0500

    primera sesion javascript

---
+++ javascript/primersesion.js
@@ -0,0 +1,72 @@
+// const usuario= {
+// 	name: "Iñaki" ,
+// 	apellidoP: "Rivas" ,
+// 	apellidoM: "Chavez" ,
+// 	edad: 200
+// }
+// console.log(dic.edad)
+
+// console.log (usuario)
+// usuario.edad = 800;
+// console.log (usuario)
+// usuario.apellidoM= "Gonzalez"
+// console.log (usuario)
+// el console.log sirve para mandar a llamar lo que dice en la terminal 
+// var iker= 
+// 	{
+// 		comoes: "enano"
+// 	}
+
+// console.log (iker.comoes)
+
+// var ola= "I'm iñaki"
+// console.log(ola);
+
+// var nombreSaludo="hola me llamo: " + nombre
+// var nombre = "iñaki"
+// var apellido = "rivas"
+// // var numString= "12345" ;
+// // console.log (numString);
+// // con un string
+// console.log (`hola me llamo: ${nombre} ${apellido}`);
+// // para hacer una variable con y el + sirve para juntar cadenas de texto
+// var a = 10;
+// var b = 100;
+// console.log (`la multiplicacion de a + b es: ${a*b}`);
+// // el signo de pesos es para unir las cadenas de texto y poder hacer operaciones
+// var c = 8000
+// var d = 100
+// console.log (`la division de c/d es: ${c/d}`) ;
+// // OPERADOR TYPEOF, tambien puedes mandar a llamar el buleano
+var conversion = `12345`
+// console.log(conversion);
+// console.log(typeof(conversion));
+var t = true;
+var f = false; 
+// console.log(`tipo de dato de la variable t ${typeof(t)}`);      
+
+conversion = String(conversion) ;
+console.log(typeof(conversion))
+
+var bool = true;
+bool = String(bool);
+
+var n = "ola";
+console.log(typeof(n));
+
+n = Number(n);
+console.log(n);
+
+console.log(+'123')
+
+
+t = Number(t)
+f = Number(f)
+console.log(t);
+console.log(typeof(t));
+console.log(f);
+console.log(typeof(f));
+
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
