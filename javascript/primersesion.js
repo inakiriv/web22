ebe988a6e0134a32f2f76b2e51c65cf09bd6ed1e
iñakooo <iako05rivas@gmail.com>
2022-08-02 11:49:06 -0500

    segundojava

--- javascript/primersesion.js
+++ javascript/primersesion.js
@@ -38,34 +38,110 @@
 // var d = 100
 // console.log (`la division de c/d es: ${c/d}`) ;
 // // OPERADOR TYPEOF, tambien puedes mandar a llamar el buleano
-var conversion = `12345`
-// console.log(conversion);
-// console.log(typeof(conversion));
-var t = true;
-var f = false; 
-// console.log(`tipo de dato de la variable t ${typeof(t)}`);      
+// var conversion = `12345`
+// // console.log(conversion);
+// // console.log(typeof(conversion));
+// var t = true;
+// var f = false; 
+// // console.log(`tipo de dato de la variable t ${typeof(t)}`);      
 
-conversion = String(conversion) ;
-console.log(typeof(conversion))
+// conversion = String(conversion) ;
+// console.log(typeof(conversion))
 
-var bool = true;
-bool = String(bool);
+// var bool = true;
+// bool = String(bool);
 
-var n = "ola";
-console.log(typeof(n));
+// var n = "ola";
+// console.log(typeof(n));
 
-n = Number(n);
-console.log(n);
+// n = Number(n);
+// console.log(n);
 
-console.log(+'123')
+// console.log(+'123')
 
 
-t = Number(t)
-f = Number(f)
-console.log(t);
-console.log(typeof(t));
-console.log(f);
-console.log(typeof(f));
+// t = Number(t)
+// f = Number(f)
+// console.log(t);
+// console.log(typeof(t));
+// console.log(f);
+// console.log(typeof(f));
+
+// var uno = 1;
+// var cero = 0;
+
+// // conversion booleana
+// // console.log (typeof(boolean(uno)))
+// console.log (`el tipo de dato de la variable uno despues de la conversion a booleano es:${typeof(Boolean(uno))}y su valor es:${Boolean(uno)}.`);
+// para mandar a llamar un booleano y saber si es verdadero o falso ya que se definen con 0 y 1 
+
+// var a = 5
+// var b = 10
+// var c = 15
+// var d = 7
+
+// console.log(a+b);
+// console.log(b-a);
+// console.log(a*c);
+// console.log(c/a);
+ 
+// console.log (`el residuo de dividir 7/5 es:${d%a}`) ;
+// // el % sirve para sacar el residuo de la divsion el cual es cero y siempre que uno quede en cero sabremos que es par 
+
+// a++ // A "a" se le aumenta uno 
+
+// c-- // A "c" se le disminuye uno 
+
+// console.log(a++)
+// console.log(a++)
+
+// el incremento y decremento es asi y uno por uno puedes agregarle uno poniendo mas a++ y a-- para restar 
+
+// var a = 5
+// var b = 10
+// var c = 15
+// var d = 7
+
+// var resultado = (3+b)*c/a*2;
+// var r2 = ((a+b*c)/5)*2;
+// var r3 = b/a+2*c;
+// var r4 = (a+b+c/c)*a;
+// var r5 = ((3*a)/c)+a+b+c;
+// var r6 = a-(b+c)*a/1;
+
+// console.log(resultado);
+// console.log(r2);
+// console.log(r3);
+// console.log(r4);
+// console.log(r5);
+// console.log(r6);
+// operaciones por jerarquia
+
+// EJERCICIO IF Y ELSE 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
